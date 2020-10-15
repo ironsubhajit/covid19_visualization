ce98e5317f97ec78e80316ea7645b4d4e7763dbc
Subhajit <mandalsubhajit330@gmail.com>
2020-10-15 21:11:50 +0530

    code to scrape coordinates data from www.quickgs.com

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import os
 import pandas as pd
 import folium
 import webbrowser
+
+if __name__ == '__main__':
+
+    # get coordinates of states
+    print("scraping coordinates data... ")
+    info = pd.read_html(
+        'http://www.quickgs.com/latitudinal-and-longitudinal-extents-of-india-indian-states-and-cities/')
